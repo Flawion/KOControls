a5d50799b9738ee9d78e329ac81f7d365289d856
Kuba Ostrowski <k-ostrowski@windowslive.com>
2018-08-04 20:47:12 +0200

    Initial Commit

---
+++ KOControls/KOControls.h
@@ -0,0 +1,19 @@
+//
+//  KOControls.h
+//  KOControls
+//
+//  Created by Kuba Ostrowski on 04.08.2018.
+//  Copyright © 2018 Kuba Ostrowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KOControls.
+FOUNDATION_EXPORT double KOControlsVersionNumber;
+
+//! Project version string for KOControls.
+FOUNDATION_EXPORT const unsigned char KOControlsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KOControls/PublicHeader.h>
+
+
